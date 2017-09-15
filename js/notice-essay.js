5741bb69c6494b3aa089cc01f3e9e71ddcf14cb9
ctt <ctt@jusfoun.com>
2017-09-15 15:31:41 +0800

    压缩版本

--- js/notice-essay.js
+++ js/notice-essay.js
@@ -1,16 +1,1 @@
-
-var id = GetQueryString('id');
-var opt = {
-  id:id
-};
-$(function(){
-  init(opt);
-});
-function init(opt) {
-  //var posturl = baseUrl+'info/info';
-  var posturl = '../js/json/notice-essay.json';
-  getdata(posturl, opt,'',fillnoticelist);//获取json数据
-  function fillnoticelist(data){
-    filldom('notice-essay', data, 'essay-box');
-  }
-}
+function init(i){function n(i){filldom("notice-essay",i,"essay-box")}getdata("../js/json/notice-essay.json",i,"",n)}var id=GetQueryString("id"),opt={id:id};$(function(){init(opt)});
